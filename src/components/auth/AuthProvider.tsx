b1809e58f488c40f570067212a443d6c59bd69cb
Yuta0227 <yutahonjo@keio.jp>
2023-09-10 23:17:15 +0900

    useAuthを修正

--- src/components/auth/AuthProvider.tsx
+++ src/components/auth/AuthProvider.tsx
@@ -1,44 +1,50 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import supabase from "../../supabase/client.tsx";
+import { Session } from "@supabase/supabase-js";
 
-type User={
-  email:string;
-  name:string;
-}
+type User = {
+  email: string;
+  name: string;
+};
 
-type AuthContextType={
-  user:User|null;
-  login:(email:string,password:string)=>Promise<void>;
-  logout:()=>Promise<void>
-}
+type AuthContextType = {
+  user: User | null;
+  login: (email: string, password: string) => Promise<void>;
+  logout: () => Promise<void>;
+};
 
-const AuthContext = createContext<AuthContextType|undefined>(undefined);
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
-export const useAuth = () => useContext(AuthContext);
-
-const AuthProvider:React.FC = ({ children }) => {
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
+const AuthProvider: React.FC = ({ children }) => {
   //ログイン時もログアウト時もセッションを更新する=>onauthchange発火
   const login = (email, password) =>
     supabase.auth.signInWithPassword({ email, password });
-  
+
   const logout = () => supabase.auth.signOut();
-  const [user, setUser] = useState(null);
-  const [session, setSession] = useState();
+  const [user, setUser] = useState<User | null>(null);
+  const [session, setSession] = useState<Session | null>();
   useEffect(() => {
     //ログイン経由でセッション更新なら
-    if(session){
+    if (session) {
       const fetchUser = async () => {
         const { data: user } = await supabase
-        .from("users")
-        .select("*")
-        .eq("email", session.user.email)
-        .single();
+          .from("users")
+          .select("*")
+          .eq("email", session.user.email)
+          .single();
         setUser(user);
       };
       fetchUser();
       //ログアウト経由でセッション更新なら
-    }else{
-      setUser(null)
+    } else {
+      setUser(null);
     }
   }, [session]);
   //ログインするとセッションが更新される
@@ -46,7 +52,7 @@ const AuthProvider:React.FC = ({ children }) => {
     const { data } = supabase.auth.onAuthStateChange((event, session) => {
       if (event === "SIGNED_IN") {
         setSession(session);
-      }else{
+      } else {
         setSession(null);
       }
     });
@@ -56,7 +62,9 @@ const AuthProvider:React.FC = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user:user as any, login:login as any, logout:logout as any }}>
+    <AuthContext.Provider
+      value={{ user: user as any, login: login as any, logout: logout as any }}
+    >
       {children}
     </AuthContext.Provider>
   );
