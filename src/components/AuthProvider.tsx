5314a412e51e6482edb4f169674b110109ec81a8
Yuta0227 <yutahonjo@keio.jp>
2023-09-17 19:23:15 +0900

    デバイス対応確認alert

--- src/components/AuthProvider.tsx
+++ src/components/AuthProvider.tsx
@@ -10,7 +10,7 @@ import OneSignal from "react-onesignal";
 type User = {
   id: string;
   email: string;
-  userRole:string;
+  userRole: string;
 };
 type Profile = {
   name: string;
@@ -50,11 +50,10 @@ const AuthProvider: React.FC<MyComponentProps> = ({ children }) => {
       throw error;
     }
   };
-  const get_my_claims=async()=>{
-    const { data, error } = await supabase
-    .rpc('get_my_claims', {});
+  const get_my_claims = async () => {
+    const { data, error } = await supabase.rpc("get_my_claims", {});
     return { data, error };
-  }
+  };
   const logout = async (): Promise<void> => {
     const { error } = await supabase.auth.signOut();
     if (error) {
@@ -86,6 +85,11 @@ const AuthProvider: React.FC<MyComponentProps> = ({ children }) => {
       }
     });
     retrieveSession();
+    //check whether device supports onesignal push
+    console.log(OneSignal.Notifications.isPushSupported().toString());
+    if(!OneSignal.Notifications.isPushSupported()){
+      alert("このデバイスは通知に対応していません。")
+    }
     return () => {
       data.subscription.unsubscribe();
     };
@@ -97,10 +101,14 @@ const AuthProvider: React.FC<MyComponentProps> = ({ children }) => {
         console.log(response.error);
         return;
       } else if (response.data.user.id && response.data.user.email) {
-        const res=await get_my_claims()
+        const res = await get_my_claims();
         //user_roleがない場合はuser_roleをuserにする
-        const userRole=res.data.user_role?res.data.user_role:'user'
-        setUser({ id: response.data.user.id, email: response.data.user.email,userRole:userRole });
+        const userRole = res.data.user_role ? res.data.user_role : "user";
+        setUser({
+          id: response.data.user.id,
+          email: response.data.user.email,
+          userRole: userRole,
+        });
         getProfile(response.data.user.id);
         //register時はエラーでない。login時はエラー出るが、気にしないでいい
         OneSignal.login(response.data.user.id).then(() => {
@@ -219,23 +227,18 @@ const AuthProvider: React.FC<MyComponentProps> = ({ children }) => {
       .catch((err) => console.error(err));
   };
   const handleNotificationPermission = async () => {
-    if(OneSignal.User.PushSubscription.optedIn){
-      OneSignal.User.PushSubscription.optOut()
-      alert('通知無許可にしました。');
-    }else{
+    if (OneSignal.User.PushSubscription.optedIn) {
+      OneSignal.User.PushSubscription.optOut();
+      alert("通知無許可にしました。");
+    } else {
       OneSignal.User.PushSubscription.optIn();
-      alert('通知許可にしました。');
+      alert("通知許可にしました。");
     }
   };
-  useEffect(()=>{
-    if(!OneSignal.User.PushSubscription.token){
-      alert('iOS16.4以上ではないと通知機能が使えません。')
-    }
-  },[])
   return (
     <AuthContext.Provider value={{ user, profile, login, logout }}>
       {children}
-      <div>通知{notification?'送信完了':''}</div>
+      <div>通知{notification ? "送信完了" : ""}</div>
       <div>エラー{errors}</div>
       <button
         onClick={() =>
@@ -249,10 +252,10 @@ const AuthProvider: React.FC<MyComponentProps> = ({ children }) => {
       <button onClick={user ? () => viewOneSignalUser(user?.id) : () => {}}>
         コンソールでユーザーの存在を確認
       </button>
-      <div>トークンの有無:{OneSignal.User.PushSubscription.token?'有':'無'}</div>
-      <button onClick={handleNotificationPermission}>
-        通知許可を変更する
-      </button>
+      <div>
+        トークンの有無:{OneSignal.User.PushSubscription.token ? "有" : "無"}
+      </div>
+      <button onClick={handleNotificationPermission}>通知許可を変更する</button>
     </AuthContext.Provider>
   );
 };
