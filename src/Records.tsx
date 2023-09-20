dd8af6801c71094a067033432cdbf38537359f49
Yuta0227 <yutahonjo@keio.jp>
2023-09-20 19:35:33 +0900

    エラー修正

--- src/Records.tsx
+++ src/Records.tsx
@@ -2,36 +2,41 @@ import { useState, useEffect } from "react";
 import supabase from "./supabase/client";
 import { formatTime } from "./components/TimeUtils";
 import { useAuth } from "./components/AuthProvider";
-//後々他のカラムを使う際にタイプ指定する必要あり
 type Record = {
   name: string;
-  user_id: string;
+  userId: string;
   id: number;
   time: string;
+  isOwner: boolean;
 };
 type UserData = {
   name: string;
-}
+};
+type NewPayload = {
+  id: number;
+  time: number;
+  user_id: string;
+};
 function Records() {
   const { user } = useAuth();
   const [records, setRecords] = useState<Record[]>([]);
-  const userCache=new Map<string,UserData>();
-  //viewではなくちゃんとテーブルを参照する。viewだと更新が反映されないかも
-  // usestateで管理したい
-  //changesをsetする
-  // https://zenn.dev/chot/articles/ddd2844ad3ae61
-  const fetchUserData = async (userId: string) => {
-    if(userCache.has(userId)){
+  const userCache = new Map<string, UserData>();
+  const fetchUserName = async (userId: string) => {
+    if (userCache.has(userId)) {
       return userCache.get(userId);
     }
-    const {data,error}=await supabase.from('profiles').select('name').eq('id',userId).single();
-    if(error){
+    const { data, error } = await supabase
+      .from("profiles")
+      .select("name")
+      .eq("user_id", userId)
+      .single();
+    if (error) {
       console.log(error);
-    }else{
-      userCache.set(userId,data);
+    } else {
+      userCache.set(userId, data);
       return data;
     }
-  }
+  };
   const fetchInitialData = async () => {
     const { data, error } = await supabase.from("records").select("*");
     if (error) {
@@ -40,14 +45,15 @@ function Records() {
       const recordsWithUserName = await Promise.all(
         data.map(async (record) => {
           // Fetch user data from the cache or database
-          const userData = await fetchUserData(record.user_id);
-
+          const userData = await fetchUserName(record.user_id);
           // Create a new object that includes the user's name
+          console.log(record.user_id === user?.id);
           return {
             id: record.id,
             time: formatTime(record.time),
-            user_id: record.user_id,
+            userId: record.user_id,
             name: userData?.name,
+            isOwner: record.user_id === user?.id,
           };
         })
       );
@@ -55,42 +61,56 @@ function Records() {
     }
   };
   useEffect(() => {
-    fetchInitialData();
-    // Create a channel and listen for real-time updates on "records" table
-    const changes = supabase
-      .channel("table-db-changes")
-      .on(
-        "postgres_changes",
-        {
-          event: "*",
-          schema: "public",
-          table: "records", // Change this to your "records" table name
-        },
-        (payload) => {
-          console.log("Change payload:", payload);
-          console.log(payload.new);
-          if (payload.new) {
-            // Handle the real-time update here
-            // You can update your state or perform any other actions as needed
-            setRecords((prev) => [...prev, payload.new as Record]);
+    if (user) {
+      fetchInitialData();
+      // Create a channel and listen for real-time updates on "records" table
+      const changes = supabase
+        .channel("table-db-changes")
+        .on(
+          "postgres_changes",
+          {
+            event: "*",
+            schema: "public",
+            table: "records", // Change this to your "records" table name
+          },
+          async (payload) => {
+            const newPayload = payload.new as NewPayload;
+            if (newPayload) {
+              const userData = await fetchUserName(newPayload.user_id);
+              // Handle the real-time update here
+              // You can update your state or perform any other actions as needed
+              if (userData) {
+                setRecords((prev) => [
+                  ...prev,
+                  {
+                    id: newPayload.id,
+                    time: formatTime(newPayload.time),
+                    userId: newPayload.user_id,
+                    name: userData.name,
+                    isOwner: newPayload.user_id === user?.id,
+                  } as Record,
+                ]);
+              }
+            }
           }
-        }
-      )
-      .subscribe();
+        )
+        .subscribe();
 
-    return () => {
-      // Unsubscribe when the component unmounts
-      changes.unsubscribe();
-    };
-  }, []);
-
+      return () => {
+        // Unsubscribe when the component unmounts
+        changes.unsubscribe();
+      };
+    }
+  }, [user]);
   return (
     <div>
       <div>記録一覧</div>
       <div>
         {records.map((record) => (
           <div key={record.id}>
-            <span>{`ID: ${record.id} 名前: ${record.name}時間: ${record.time}`}</span>
+            <span
+              style={{ color: record.isOwner ? "red" : "black" }}
+            >{`ID: ${record.id} 名前: ${record.name}時間: ${record.time}`}</span>
           </div>
         ))}
       </div>
